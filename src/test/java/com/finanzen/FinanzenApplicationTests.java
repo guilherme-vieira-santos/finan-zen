2ca9e86b3ca5aa91f36677ea3df864abff92f66e
Guilherme Vieira Santos <guilhermebusiness7@gmail.com>
2025-09-27 21:24:50 -0300

    iniciando projeto
    iniciando projeto

---
+++ src/test/java/com/finanzen/FinanzenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanzen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
